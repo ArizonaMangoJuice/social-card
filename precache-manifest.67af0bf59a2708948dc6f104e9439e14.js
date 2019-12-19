540329294a303031ac4cbcc4afc9218480f1249d
isaelwebdeveloper@gmail.com <isaelwebdeveloper@gmail.com>
2019-12-19 12:30:55 -0700

    Updates

---
+++ precache-manifest.67af0bf59a2708948dc6f104e9439e14.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6154eddaadd0608bcbc7f1d624db155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c0403da5ec033f376e7",
+    "url": "/static/css/main.61b5b564.chunk.css"
+  },
+  {
+    "revision": "6fc10a49a16bd23933ba",
+    "url": "/static/js/2.c85cf727.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c85cf727.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c0403da5ec033f376e7",
+    "url": "/static/js/main.ff90d927.chunk.js"
+  },
+  {
+    "revision": "e99db38954e620ef4560",
+    "url": "/static/js/runtime-main.6ff47d38.js"
+  },
+  {
+    "revision": "747a2a272f8a9b544d1f6475bb163003",
+    "url": "/static/media/background.747a2a27.jpg"
+  },
+  {
+    "revision": "676968b505254456a8a98ee6dcc6bcf5",
+    "url": "/static/media/email.676968b5.png"
+  },
+  {
+    "revision": "623e44b54fddd0eb6af9d45c27dcef63",
+    "url": "/static/media/isael.623e44b5.jpg"
+  },
+  {
+    "revision": "6e25bee3de95301fff966559dda891e3",
+    "url": "/static/media/location.6e25bee3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/static/media/user.7ea4af7d.png"
+  },
+  {
+    "revision": "8646ac2ca6ba17c0bc454bd5ad297de1",
+    "url": "/static/media/web.8646ac2c.png"
+  }
+]);
